5f5c2ea615fca23e0089de1e99c32ec60305003b
miscuff <alexandrecuffel@gmail.com>
2020-03-30 18:13:30 +0200

    add homepage and base

--- bp_project/bp_project/settings.py
+++ bp_project/bp_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'bp_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,5 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 INTERNAL_IPS = ['127.0.0.1']
---
+++ bp_project/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.template import loader
+
+
+def home(request):
+    return render(request, 'home/accueil.html', locals())
+
+
