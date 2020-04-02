6f613083b3cd461bf1c77d086c4e62b9412e07a8
miscuff <alexandrecuffel@gmail.com>
2020-04-02 16:28:19 +0200

    Add forms et views

--- bp_project/account/forms.py
+++ bp_project/account/forms.py
@@ -1,12 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class ConnexionForm(forms.Form):
     username = forms.CharField(label="Votre nom d'utilisateur", max_length=30)
-    email = forms.EmailField(label="Votre adresse e-mail")
-
-"""
-class CreationForm(forms.Form)
     password = forms.CharField(label="Votre mot de passe", max_length=10,
                                widget=forms.PasswordInput)
-"""
+
+
+class CreationForm(forms.Form):
+    username = forms.CharField(label="Votre nom d'utilisateur", max_length=30)
+    email = forms.EmailField(label="Votre adresse e-mail")
+    password = forms.CharField(label="Votre mot de passe", max_length=10,
+                               widget=forms.PasswordInput)
+
+
+def clean_email(self):
+    email = cleaned_data.get('email')
+    if User.objects.filter(email=self.email).exists():
+        raise forms.ValidationError("Cette email est déjà utilisé")
--- bp_project/account/views.py
+++ bp_project/account/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.forms import ValidationError
 
-from .forms import ConnexionForm
+from .forms import ConnexionForm, CreationForm
 
 
 def connexion(request):
@@ -10,9 +13,26 @@ def connexion(request):
         email = form.cleaned_data['email']
         password = form.cleaned_data['password']
         envoi = True
+        """
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return an 'invalid login' error message.         
+             """
     # Quoiqu'il arrive, on affiche la page du formulaire.
     return render(request, 'account/connexion.html', locals())
 
 
 def account_creation(request):
+    form = CreationForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        User.objects.create_user(username, email, password)
+        envoi = True
     return render(request, 'account/creation.html', locals())
+
+
