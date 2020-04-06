91bb3aefc6018ebb75a61459f770df8fcf0f2679
miscuff <alexandrecuffel@gmail.com>
2020-04-06 15:23:36 +0200

    creation models products

--- bp_project/products/models.py
+++ bp_project/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    nutriscore_grade = models.CharField(max_length=1, null=True)
+    store = models.CharField(max_length=50, null=True)
+    url_picture = models.URLField()
+    url_product = models.URLField()
+    description = models.TextField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Substitute(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product, related_name='substitutes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
