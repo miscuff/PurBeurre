6d33b8992a63a013db21a700bf9e1c1823348891
miscuff <alexandrecuffel@gmail.com>
2020-04-13 18:19:07 +0200

    add method resultats

--- bp_project/products/views.py
+++ bp_project/products/views.py
@@ -8,10 +8,18 @@ def search(request):
     search_products = request.POST.get('search_products').lower()
     products_db = Product.objects.filter(product_name__istartswith=search_products).values()
     context = {
-        'products': products_db
+        'products': products_db,
+        'search_products': search_products
     }
     return render(request, 'products/aliments.html', context)
 
 
 def detail(request, product_id):
-    pass
+    product_chosen = Product.objects.get(pk=product_id)
+    products = product_chosen.objects.filter.select_related('category')
+    substitutes = [product for product in products if products['nutriscore_grade'] < product_chosen['nutriscore_grade']]
+    context = {
+        'product': product_chosen,
+        'substitutes': substitutes,
+    }
+    return render(request, 'store/resultats.html', context)
