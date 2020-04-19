222fea666862d8ded0663acbc0d17ce4df967386
miscuff <alexandrecuffel@gmail.com>
2020-04-19 10:28:47 +0200

    remove staticfilesdir

--- bp_project/settings.py
+++ bp_project/settings.py
@@ -138,10 +138,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 INTERNAL_IPS = ['127.0.0.1']
 
 LOGIN_REDIRECT_URL = '/account/account/'
