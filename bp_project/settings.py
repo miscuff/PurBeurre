b1984bfb05ec60bef0f3a5a39998f4fcb32cc1bb
miscuff <alexandrecuffel@gmail.com>
2020-04-19 11:02:25 +0200

    use base_dir instead of project_root

--- bp_project/settings.py
+++ bp_project/settings.py
@@ -152,11 +152,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(BASE_DIR, 'static'),
     )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
