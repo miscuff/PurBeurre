6b9db93b6b37e231d40b2ebb9e72260651d96d0d
miscuff <alexandrecuffel@gmail.com>
2020-04-20 15:58:19 +0200

    add test for products

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from account.forms import LoginForm, CreationForm
 from products.models import Category, Product, Substitute
@@ -13,7 +14,7 @@ class HomePageTestCase(TestCase):
         response = self.client.get(reverse('home:home'))
         self.assertEqual(response.status_code, 200)
 
-"""
+
 # Account app
 class AccountPageTestCase(TestCase):
 
@@ -38,41 +39,56 @@ class AccountPageTestCase(TestCase):
     # test account_creation
     def test_account_creation(self):
         pass
-    
-"""
+
+
 # Products app
 class ProductsPageTestCase(TestCase):
 
     def setUp(self):
         self.category = Category.objects.create(name='petit-dejeuners')
         self.c1 = Category.objects.get(name='petit-dejeuners')
-        self.product = Product.objects.create(name='nutella',
-                                              nutriscore_grade='b',
+        self.product = Product.objects.create(product_name='nutella',
+                                              nutriscore_grade='d',
                                               url_picture='http://url_picture.fr',
                                               url_product='http://url_product.fr',
-                                              category=self.c1.pk)
-        self.p1 = Product.objects.get(name='nutella')
+                                              category=self.category)
+        self.p1 = Product.objects.get(product_name='nutella')
+        self.user = User.objects.create_user(username='john',
+                                 email='jlennon@beatles.com',
+                                 password='glass onion')
+        self.u1 = User.objects.get(username='john')
+        self.substitute = Substitute.objects.create(id=self.p1.id,
+                                                    created_at=timezone.now(),
+                                                    user_id=self.u1.id)
+        self.s1 = Substitute.objects.get(user_id=self.u1)
 
     # test the list of products
     def test_display_product(self):
-        response = self.client.get(reverse('products:search'))
+        product_name = self.p1.product_name
+        response = self.client.post(reverse('products:search'),
+                                    {'search_products': product_name})
         self.assertEqual(response.status_code, 200)
 
-    """
     # test details page
     def test_details(self):
-        pass
+        product_id = self.p1.id
+        response = self.client.get(reverse('products:detail',
+                                            args=(product_id,)))
+        self.assertEqual(response.status_code, 200)
 
     # test the list of substitutes
     def test_display_substitutes(self):
-        pass
+        product_id = self.p1.id
+        response = self.client.get(reverse('products:substitute',
+                                            args=(product_id,)))
+        self.assertEqual(response.status_code, 200)
 
-    # test the sub is saved
+    # test the sub is saved it will add one row to database
     def test_save_substitutes(self):
+        substitute_id = self.u1.id
         pass
 
     # test the list of favorites
     def test_display_favorites(self):
-        pass
-
-"""
+        response = self.client.get(reverse('products:show_favorites'))
+        self.assertEqual(response.status_code, 200)
