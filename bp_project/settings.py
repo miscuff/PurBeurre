40511f6012f21067a967d5cfed73d56393464b7a
miscuff <alexandrecuffel@gmail.com>
2020-04-20 09:08:35 +0200

    add reportory to project root

--- bp_project/settings.py
+++ bp_project/settings.py
@@ -149,13 +149,13 @@ LOGIN_URL = '/account/connexion/'
 
 if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(PROJECT_ROOT, 'bp_project/static'),
     )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
