d84563373d6705294fd737f521e722631cbbb44d
miscuff <alexandrecuffel@gmail.com>
2020-05-02 11:54:35 +0200

    add pytest

--- bp_project/settings.py
+++ bp_project/settings.py
@@ -176,5 +176,5 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=home, account, products',
+    '--cover-package=home, products, account',
     ]
